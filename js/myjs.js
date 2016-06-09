e0ef2269ab4ad0a9855caf9b26865de681a5b1dc
jakemcginnz <jakemcginnz@gmail.com>
2016-06-09 11:28:41 -0400

    Update myjs.js

--- js/myjs.js
+++ js/myjs.js
@@ -43,3 +43,14 @@ function SignOutUser() {
   firebase.auth().signOut();
   console.log("signed Out")
 }
+var user = firebase.auth().currentUser;
+
+if (user != null) {
+  user.providerData.forEach(function (profile) {
+    console.log("Sign-in provider: "+profile.providerId);
+    console.log("  Provider-specific UID: "+profile.uid);
+    console.log("  Name: "+profile.displayName);
+    console.log("  Email: "+profile.email);
+    console.log("  Photo URL: "+profile.photoURL);
+  });
+}
